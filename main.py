9335140aec9bb8a8679ed78d499b28dbaba660ef
edurs2602 <edurs.2602@gmail.com>
2019-11-25 16:03:38 -0300

    Screen + RV

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import sqlite3
 from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
 from kivy.uix.screenmanager import Screen, ScreenManager
 from kivy.uix.spinner import Spinner
 from kivy.config import Config
@@ -15,13 +16,21 @@ from kivy.uix.popup import Popup
 from kivy.uix.scrollview import ScrollView
 from kivy.uix.image import Image
 from kivy.animation import Animation
-from kivy.properties import ObjectProperty, ListProperty, StringProperty
+from kivy.uix.recycleview import RecycleView
+from kivy.lang import Builder
+from kivy.properties import ObjectProperty, ListProperty, StringProperty, BooleanProperty
+from kivy.uix.recyclegridlayout import RecycleGridLayout
+from kivy.uix.behaviors import FocusBehavior
+from kivy.uix.recycleview.layout import LayoutSelectionBehavior
+from kivy.uix.recycleview.views import RecycleDataViewBehavior
+from kivy.uix.recycleboxlayout import RecycleBoxLayout
+from kivy.uix.recyclegridlayout import RecycleGridLayout
+from kivy.uix.behaviors import FocusBehavior
 from kivy.core.window import Window
 
 Config.set('kivy', 'exit_on_escape', 0)
 Config.set(u'''graphics''', u''''resizable''', True)
 Config.write()
-Window.size = (800, 600)
 
 connection = sqlite3.connect('teste.db')
 cursor = connection.cursor()
@@ -35,7 +44,6 @@ class Screenmanager(ScreenManager):         #Tela que comanda todas as outras
 class Screen1(Screen):
 
     def confirmacao(self, *args):           #Criar um pop-up para perguntar se o usuario desejar sair
-
         box = BoxLayout(orientation='vertical', padding=10, spacing=10)
         botoes = BoxLayout(padding=10, spacing=10)
 
@@ -43,7 +51,7 @@ class Screen1(Screen):
                     size=(240, 160))
 
         sim = Button(text='Sim', on_release=app.get_running_app().stop)
-        nao = Button(text= 'Não', on_release=pop.dismiss)
+        nao = Button(text='Não', on_release=pop.dismiss)
 
         botoes.add_widget(sim)
         botoes.add_widget(nao)
@@ -80,33 +88,80 @@ class ScreenCDF(Screen):
         cursor.execute(f'''INSERT INTO dados VALUES(NULL, '{Nome}', '{Diretor}', '{Lancamento}', '{Valor}', '{Nota}', '{Genero}')''')
         connection.commit()
 
-    def clear_TI(self):
-        self.ids.ti_nome.text = ''
-        self.ids.ti_diretor.text = ''
-        self.ids.ti_lancamento.text = ''
-        self.ids.ti_valor.text = ''
-        self.ids.ti_nota.text = ''
-        self.ids.spinner_id.text = 'Selecione um Genero'
+    def salvo(self, *args):
+        box = BoxLayout(orientation='vertical', padding=10, spacing=10)
+        botoes = BoxLayout(padding=10, spacing=10)
 
+        pop = Popup(title='Cadastrado com sucesso!', content=box, size_hint=(None, None),
+                    size=(240, 160))
+
+        ok = Button(text='OK', on_press=pop.dismiss, on_release=self.changer)
+
+        botoes.add_widget(ok)
+
+        atencao = Image(source='ok.png')
+
+        box.add_widget(atencao)
+        box.add_widget(botoes)
+
+        anim = Animation(size=(300, 220), duration=0.2, t='out_circ')
+        anim.start(pop)
+
+        pop.open()
+
+    def changer(self, *args):
+        self.manager.current = 'Screen2'
 
 class Screen_Listar(Screen):
-    rows = ListProperty([('id','Nome','Diretor','Lancamento','Valor','Nota','Genero')])
+    pass
+
+class SelectableRecycleGridLayout(FocusBehavior, LayoutSelectionBehavior, RecycleGridLayout):
+    ''' Adds selection and focus behaviour to the view. '''
+
+class SelectableButton(RecycleDataViewBehavior, Button):
+    index = None
+    selected = BooleanProperty(False)
+    selectable = BooleanProperty(True)
+
+    def refresh_view_attrs(self, rv, index, data):
+        ''' Catch and handle the view changes '''
+        self.index = index
+        return super(SelectableButton, self).refresh_view_attrs(
+            rv, index, data)
+
+    def on_touch_down(self, touch):
+        ''' Add selection on touch down '''
+        if super(SelectableButton, self).on_touch_down(touch):
+            return True
+        if self.collide_point(*touch.pos) and self.selectable:
+            return self.parent.select_with_touch(self.index, touch)
+
+    def apply_selection(self, rv, index, is_selected):
+        ''' Respond to the selection of items in the view. '''
+        self.selected = is_selected
+        if is_selected:
+            print("selection changed to {0}".format(rv.data[index]))
+        else:
+            print("selection removed for {0}".format(rv.data[index]))
+
+class RV(RecycleView ,Screen):
+    data_list = ListProperty([])
+
+    def __init__(self, **kwargs):
+        super(RV, self).__init__(**kwargs)
+        self.data_transfer()
 
 
-    def att_data(self):
-        self.connection = sqlite3.connect('teste.db')
-        self.cursor = connection.cursor()
+    def data_transfer(self):
+        connection = sqlite3.connect('teste.db')
+        cursor = connection.cursor()
 
-        cursor.execute('''SELECT * FROM dados''')
-        for i in cursor:
-            r1 = 'ID: '+str(100000000+i[0])[1:9]+'\n'
-            r2 = 'Nome: ' + str(i[1]) + '\n'
-            r3 = 'Diretor: ' + str(i[2]) + '\n'
-            r4 = 'Lancamento: ' + str(i[3]) + '\n'
-            r5 = 'Valor: ' + str(i[4]) + '\n'
-            r6 = 'Nota: ' + str(i[5]) + '\n'
-            r7 = 'Genero: ' + str(i[6]) + '\n'
-            listar = r1 + r2 + r3 + r4 + r5 + r6 + r7
+        cursor.execute('''SELECT * FROM dados ORDER BY Nome ASC''')
+        row = cursor.fetchall()
+
+        for rows in row:
+            for col in rows:
+                self.data_list.append(col)
 
 class app(App):
     title = "Cadastro de Filmes"
